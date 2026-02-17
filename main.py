e4888f45196ae51d524f7b6c2630a4168ccbdada
lazyorangejuice <melvinb35@hotmail.com>
2026-02-16 21:19:16 -0500

    add black screen and quit event

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import pygame
 from constants import *
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return  # Handle events here
+    screen.fill("black")  # Clear the screen with black
+    pygame.display.flip()  # Update the display
 
 if __name__ == "__main__":
     main()
+
